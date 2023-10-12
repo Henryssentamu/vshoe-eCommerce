7399dceab58936d12d118f9b0435226e996d418f
Henryssentamu <ssentamuhenry61@gmail.com>
2023-10-12 17:16:05 +0300

    shoes data file

---
+++ data/maleShoes.js
@@ -0,0 +1,48 @@
+const maleshoeData = [
+    {
+        id: "v1",
+        image:"../pictures/518pqQTptyL._AC_UY575_.jpg",
+        name:"Under Armour Women's Surge 3 Running Shoe",
+        price:10343
+    },
+
+    {
+        id: "v2",
+        image:"../pictures/717BYV3pMbL._AC_SX575._SX._UX._SY._UY_.jpg",
+        name:"Skechers Men's Go Max-Athletic Air Mesh Slip on Walking Shoe Sneaker",
+        price:5995
+    },
+
+    {
+        id: "v3",
+        image:"../pictures/51kIWvgxvGL._AC_UY575_.jpg",
+        name:"Under Armour Women's HOVR Turbulence 2 Running Shoe",
+        price:60000
+    },
+
+    {
+        id: "v4",
+        image:"../pictures/51H5JtzTzzL._AC_UY575_.jpg",
+        name:"Under Armour Unisex-Child Grade School Lockdown 6 Basketball Shoe",
+        price:4112
+    },
+
+    {
+        id: "v5",
+        image:"../pictures/51H5JtzTzzL._AC_UY575_.jpg",
+        name:"Under Armour Unisex-Child Grade School Lockdown 6 Basketball Shoe",
+        price:4112
+    },
+    {
+        id: "v6",
+        image:"../pictures/51tXau4029L._AC_SY575._SX._UX._SY._UY_.jpg",
+        name:"Under Armour Unisex-Child Grade School Lockdown 6 Basketball Shoe",
+        price:4212
+    },
+    {
+        id: "v7",
+        image:"../pictures/51WzQRbpYmL._AC_SY575._SX._UX._SY._UY_.jpg",
+        name:"Under Armour Unisex-Child Grade School Lockdown 6 Basketball Shoe",
+        price:4212
+    }
+]
